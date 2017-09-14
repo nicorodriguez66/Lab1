553c34988da4001bd778a9dfdcd8e61379e550a8
nicorodriguez66 <nicorodriguez66@gmail.com>
2017-09-14 15:41:56 -0300

    Initial Commit
    Solution
    Proyect Class Library
    Proyect Graphics
    Windows Forms with dropdown menu

---
+++ Obligatorio1/SchoolERP.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Obligatorio1
+{
+    partial class SchoolERP
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newSubjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editSubjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteSubjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teachersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newTeacherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editTeacherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteTeacherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vansToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newVanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editVanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteVanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.activityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newActivityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editActivityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteActivityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.studentsToolStripMenuItem,
+            this.teachersToolStripMenuItem,
+            this.vansToolStripMenuItem,
+            this.activityToolStripMenuItem,
+            this.feeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(490, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newSubjectToolStripMenuItem,
+            this.editSubjectToolStripMenuItem,
+            this.deleteSubjectToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(63, 20);
+            this.toolStripMenuItem1.Text = "Subjects";
+            // 
+            // newSubjectToolStripMenuItem
+            // 
+            this.newSubjectToolStripMenuItem.Name = "newSubjectToolStripMenuItem";
+            this.newSubjectToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.newSubjectToolStripMenuItem.Text = "New Subject";
+            // 
+            // editSubjectToolStripMenuItem
+            // 
+            this.editSubjectToolStripMenuItem.Name = "editSubjectToolStripMenuItem";
+            this.editSubjectToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.editSubjectToolStripMenuItem.Text = "Edit Subject";
+            // 
+            // deleteSubjectToolStripMenuItem
+            // 
+            this.deleteSubjectToolStripMenuItem.Name = "deleteSubjectToolStripMenuItem";
+            this.deleteSubjectToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.deleteSubjectToolStripMenuItem.Text = "Delete Subject";
+            // 
+            // studentsToolStripMenuItem
+            // 
+            this.studentsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newStudentToolStripMenuItem,
+            this.editStudentToolStripMenuItem,
+            this.deleteStudentToolStripMenuItem});
+            this.studentsToolStripMenuItem.Name = "studentsToolStripMenuItem";
+            this.studentsToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.studentsToolStripMenuItem.Text = "Students";
+            // 
+            // newStudentToolStripMenuItem
+            // 
+            this.newStudentToolStripMenuItem.Name = "newStudentToolStripMenuItem";
+            this.newStudentToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.newStudentToolStripMenuItem.Text = "New Student";
+            // 
+            // editStudentToolStripMenuItem
+            // 
+            this.editStudentToolStripMenuItem.Name = "editStudentToolStripMenuItem";
+            this.editStudentToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.editStudentToolStripMenuItem.Text = "Edit Student";
+            // 
+            // deleteStudentToolStripMenuItem
+            // 
+            this.deleteStudentToolStripMenuItem.Name = "deleteStudentToolStripMenuItem";
+            this.deleteStudentToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.deleteStudentToolStripMenuItem.Text = "Delete Student";
+            // 
+            // teachersToolStripMenuItem
+            // 
+            this.teachersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newTeacherToolStripMenuItem,
+            this.editTeacherToolStripMenuItem,
+            this.deleteTeacherToolStripMenuItem});
+            this.teachersToolStripMenuItem.Name = "teachersToolStripMenuItem";
+            this.teachersToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.teachersToolStripMenuItem.Text = "Teachers";
+            // 
+            // newTeacherToolStripMenuItem
+            // 
+            this.newTeacherToolStripMenuItem.Name = "newTeacherToolStripMenuItem";
+            this.newTeacherToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newTeacherToolStripMenuItem.Text = "New Teacher";
+            // 
+            // editTeacherToolStripMenuItem
+            // 
+            this.editTeacherToolStripMenuItem.Name = "editTeacherToolStripMenuItem";
+            this.editTeacherToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.editTeacherToolStripMenuItem.Text = "Edit Teacher";
+            // 
+            // deleteTeacherToolStripMenuItem
+            // 
+            this.deleteTeacherToolStripMenuItem.Name = "deleteTeacherToolStripMenuItem";
+            this.deleteTeacherToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deleteTeacherToolStripMenuItem.Text = "Delete Teacher";
+            // 
+            // vansToolStripMenuItem
+            // 
+            this.vansToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newVanToolStripMenuItem,
+            this.editVanToolStripMenuItem,
+            this.deleteVanToolStripMenuItem});
+            this.vansToolStripMenuItem.Name = "vansToolStripMenuItem";
+            this.vansToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.vansToolStripMenuItem.Text = "Vans";
+            // 
+            // newVanToolStripMenuItem
+            // 
+            this.newVanToolStripMenuItem.Name = "newVanToolStripMenuItem";
+            this.newVanToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.newVanToolStripMenuItem.Text = "New Van";
+            // 
+            // editVanToolStripMenuItem
+            // 
+            this.editVanToolStripMenuItem.Name = "editVanToolStripMenuItem";
+            this.editVanToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.editVanToolStripMenuItem.Text = "Edit Van";
+            // 
+            // deleteVanToolStripMenuItem
+            // 
+            this.deleteVanToolStripMenuItem.Name = "deleteVanToolStripMenuItem";
+            this.deleteVanToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.deleteVanToolStripMenuItem.Text = "Delete Van";
+            // 
+            // activityToolStripMenuItem
+            // 
+            this.activityToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newActivityToolStripMenuItem,
+            this.editActivityToolStripMenuItem,
+            this.deleteActivityToolStripMenuItem});
+            this.activityToolStripMenuItem.Name = "activityToolStripMenuItem";
+            this.activityToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.activityToolStripMenuItem.Text = "Activities";
+            // 
+            // newActivityToolStripMenuItem
+            // 
+            this.newActivityToolStripMenuItem.Name = "newActivityToolStripMenuItem";
+            this.newActivityToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.newActivityToolStripMenuItem.Text = "New Activity";
+            // 
+            // editActivityToolStripMenuItem
+            // 
+            this.editActivityToolStripMenuItem.Name = "editActivityToolStripMenuItem";
+            this.editActivityToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.editActivityToolStripMenuItem.Text = "Edit Activity";
+            // 
+            // deleteActivityToolStripMenuItem
+            // 
+            this.deleteActivityToolStripMenuItem.Name = "deleteActivityToolStripMenuItem";
+            this.deleteActivityToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.deleteActivityToolStripMenuItem.Text = "Delete Activity";
+            // 
+            // feeToolStripMenuItem
+            // 
+            this.feeToolStripMenuItem.Name = "feeToolStripMenuItem";
+            this.feeToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.feeToolStripMenuItem.Text = "Fees";
+            // 
+            // SchoolERP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 262);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SchoolERP";
+            this.Text = "School-ERP";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newSubjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editSubjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteSubjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newStudentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editStudentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteStudentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teachersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newTeacherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editTeacherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteTeacherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vansToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newVanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editVanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteVanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem activityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newActivityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editActivityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteActivityToolStripMenuItem;
+    }
+}
+
